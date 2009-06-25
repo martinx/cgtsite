c3a2a4f2ea30ad07de82877912a23e6085c35cdc
xiaoqiu369 <xiaoqiu369@8741030e-4907-11de-80ae-c1ff141340a0>
2009-06-25 12:58:22 +0000

    后台页面修改

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class BaseController < ApplicationController
+
+  puts "===============================this is come from baseController======================================"
+
+end
